a99a723ea9437589930475087b8ae2d494a433be
grogg19 <differ@list.ru>
2021-07-27 00:03:48 +0300

    добавлена команда RepeatMessageCommand в приложение

--- app.php
+++ app.php
@@ -3,6 +3,9 @@
 require_once 'bootstrap.php';
 
 use Symfony\Component\Console;
+use App\Console\RepeatMessage\RepeatMessageCommand;
 
-$app = new Console\Application('Some');
+$app = new Console\Application('Repeat Message');
+$app->add(new RepeatMessageCommand());
+
 $app->run();
