c420df139eb27ddde78cb6aaeea48d6048d6ddb3
grogg19 <differ@list.ru>
2021-07-27 02:45:11 +0300

    delete name

--- app.php
+++ app.php
@@ -5,7 +5,6 @@ require_once 'bootstrap.php';
 use Symfony\Component\Console;
 use App\Console\Questions\QuestionsCommand;
 
-$app = new Console\Application('Ask Questions');
-
+$app = new Console\Application();
 $app->add(new QuestionsCommand());
 $app->run();
