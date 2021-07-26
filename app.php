83b571caf666d1070c90aa38beb44e0a9e623b5a
grogg19 <differ@list.ru>
2021-07-27 02:43:09 +0300

    fix after merge

--- app.php
+++ app.php
@@ -5,15 +5,12 @@ require_once 'bootstrap.php';
 use Symfony\Component\Console;
 use App\Console\SayHello\SayHelloCommand;
 use App\Console\RepeatMessage\RepeatMessageCommand;
-
-$app = new Console\Application('Say Hello');
-$app->add(new SayHelloCommand('say_hello'));
 use App\Console\Questions\QuestionsCommand;
 
-$app = new Console\Application('Repeat Message');
+$app = new Console\Application('Big program');
+
+$app->add(new SayHelloCommand());
 $app->add(new RepeatMessageCommand());
-
-$app = new Console\Application('Ask Questions');
-
 $app->add(new QuestionsCommand());
+
 $app->run();
